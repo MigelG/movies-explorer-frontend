9e08a854d63221879a0141db46bba9742352713a
MigelG <za4elovek@yandex.ru>
2022-03-26 15:45:44 +0300

    Добавить маршруты

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,36 @@
+import { useState, useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
+import Login from '../Login/Login';
 import Main from '../Main/Main';
+import Movies from '../Movies/Movies';
+import Profile from '../Profile/Profile';
+import Register from '../Register/Register';
+import SavedMovies from '../SavedMovies/SavedMovies';
 import './App.css';
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    loggedIn ? navigate('/') : navigate('/sign-in');
+  }, [loggedIn]);
+
   return (
     <div className="App">
       <Header />
-      <Main />
+
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/movies' element={<Movies />} />
+        <Route path='/saved-movies' element={<SavedMovies />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/signin' element={<Login />} />
+        <Route path='/signup' element={<Register />} />
+      </Routes>
+
       <Footer />
     </div>
   );
